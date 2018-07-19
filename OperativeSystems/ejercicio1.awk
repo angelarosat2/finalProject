33f66bca3ad4f3c14ce13dd6c16ed34b0ba50a2b
Angela <angela.rosa@anima.edu.uy>
2018-07-19 12:39:25 -0300

    Ejercicio 1 casi terminado

--- OperativeSystems/ejercicio1.awk
+++
@@ -1,8 +0,0 @@
-#Para ejecutar este código debe ingresar: last -w | awk -v user=$USER -f ejercicio1.awk
-
-BEGIN {
-    printf "Ahora verá los login y logout del usuario %s \n", user
-}
-{
-    printf $1 " " $4 " " $5 " " $6 " " $7 "\n" 
-}
---
+++ OperativeSystems/ejercicio1.bash
@@ -0,0 +1,22 @@
+#Para ejecutar este código debe ingresar: bash ejercicio1.bash
+
+!#/bin/bash 
+echo "Desea filtrar los login y logout por nombre de usuario? S/N"
+read respuesta
+
+if [ $respuesta = "s" ]
+then
+    echo "Ingrese un usuario"
+    read user
+    while [ -z "$user" ]; do
+        echo "Debe ingresar un usuario"
+        read user
+    done
+    last -w $user
+    last -f /var/log/wtmp.1 -w $user
+elif [ $respuesta = "n" ]
+then
+    last
+else
+    echo "Opción inválida"
+fi
